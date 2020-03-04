950ca422fe33b9d35e9bdaba7c89708dca7e7c49
Kohei Asai <yo@kohei.dev>
2020-03-04 02:53:02 -0800

    Add tests for Firestore rules

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  globalSetup: "<rootDir>/.jest/firestoreGlobalSetup.js",
+  globalTeardown: "<rootDir>/.jest/firestoreGlobalTeardown.js",
   testPathIgnorePatterns: ["/node_modules/", "/.functions/"]
 };
