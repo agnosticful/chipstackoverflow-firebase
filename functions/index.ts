e10c62c402c5967af8000a20d850bdacd6fe8e30
Kohei Asai <yo@kohei.dev>
2020-03-06 00:34:33 -0800

    Add onUserReactionDeleted() Firestore trigger function

--- functions/index.ts
+++ functions/index.ts
@@ -1,3 +1,4 @@
 export { default as onUserCreated } from "./functions/onUserCreated";
 export { default as onUserReactionCreated } from "./functions/onUserReactionCreated";
+export { default as onUserReactionDeleted } from "./functions/onUserReactionDeleted";
 export { default as ping } from "./functions/ping";
