16c366a1685024406a251fd52d29bc6532b795e8
Kohei Asai <yo@kohei.dev>
2020-03-06 00:34:19 -0800

    Add onUserReactionCreated() Firestore trigger function

--- functions/index.ts
+++ functions/index.ts
@@ -1,2 +1,3 @@
 export { default as onUserCreated } from "./functions/onUserCreated";
+export { default as onUserReactionCreated } from "./functions/onUserReactionCreated";
 export { default as ping } from "./functions/ping";
