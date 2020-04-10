cbd79198db57b1274af5ed10cabeef0f089aea61
Kohei Asai <yo@kohei.dev>
2020-04-09 22:14:53 -0700

    Add GameStreetAction#type (#44)

--- functions/types/GameStreetAction.ts
+++ functions/types/GameStreetAction.ts
@@ -1,4 +1,13 @@
 export default interface GameStreetAction {
   playerIndex: number;
+  type: GameStreetActionType;
   betSize: number;
+}
+
+export enum GameStreetActionType {
+  fold = "FOLD",
+  check = "CHECK",
+  call = "CALL",
+  bet = "BET",
+  raise = "RAISE",
 }
