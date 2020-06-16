580d4c41e0ee6d40b309315dd7401d285d068f93
Kohei Asai <yo@kohei.dev>
2020-06-15 22:18:41 -0700

    Fix wrong module path

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 export { default as graphql } from "./graphql";
-export { default as onAuthenticate } from "./on-authenticate";
 export { default as onCreateAnswerReaction } from "./on-create-answer-reaction";
 export { default as onCreateCommentReaction } from "./on-create-comment-reaction";
 export { default as onDeleteAnswerReaction } from "./on-delete-answer-reaction";
 export { default as onDeleteCommentReaction } from "./on-delete-comment-reaction";
+export { default as onRegisterUser } from "./on-register-user";
